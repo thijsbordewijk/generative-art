df890c65280c0d36bd0ac408bf0c4f0411c87ad6
thijsbordewijk <43644032+thijsbordewijk@users.noreply.github.com>
2021-05-18 16:48:25 +0200

    final update

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,44 +1,35 @@
-// /* 👇 Start writing your p5.js code here */
-// let note;
-
 
 // variabelen aanmaken.
 var button = document.createElement("a");
 var volBlock = document.createElement("p");
 var buttonSpot = document.getElementsByClassName("buttons")[0];
 var x, y, w, h;
-var buttonText = "add shape";
-let mic;
+var mic;
+
 
 function setup() {
 
-// grijs canvas ten grootte van het scherm, met bovenaan een knop die de microfoon simuleert.
-createCanvas(1900, 885);
-background(240);
-
-mic = new p5.AudioIn();
-mic.start();
-
-function draw() {
-vol = mic.getLevel();
-console.log(vol)
+  // grijs canvas ten grootte van het scherm, met bovenaan een knop die de microfoon simuleert.
+  createCanvas(1900, 885);
+  background(240);
+  mic = new p5.AudioIn();
+  mic.start();
+  frameRate(10);
 }
 
-button.innerHTML = buttonText;
-buttonSpot.appendChild(button);
-button.addEventListener ("click",
 
-// de functie van het aanmaken van een random vorm zit binnen de evenListener van de knop zodat deze alleen activeert als de knop wordt ingedrukt.
 function drawMore() {
-  drawRandomShape("ellipse");
 
+  drawRandomShape("ellipse");
+  
   // de plek en grootte van de cirkel wordt random uitgekozen
   function drawRandomShape(choice) {
     x = random(width);
     y = random(height);
-    w = random(5, 200);
-    h = random(5, 200);
+    w = random(100-300);
+    h = random(100-300);
     
+      
     // de cirkel wordt geplaatst met een random kleurwaarde en een opacity van 75%
     if (choice == "ellipse") {
       noStroke();
@@ -46,5 +37,23 @@ function drawMore() {
       ellipse(x, y, w, h);
     }
   }
-})
 }
+
+function draw() {
+  vol = mic.getLevel();
+  var volVar = vol * 100;
+  console.log(volVar)
+
+  if (volVar > 20) {
+    drawMore()
+  }
+}
+  
+button.innerHTML = "reset canvas";
+buttonSpot.appendChild(button);
+button.addEventListener ("click",
+  function resetCanvas() {
+    createCanvas(1900, 885);
+    background(240);
+  }
+);
